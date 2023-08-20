eb5debb17602a0a4c4ec1ca5f2a1206e090bc890
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-08-20 20:58:48 +0900

    adding prices listing

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+let count = 0; // Initialize the count
+
+function clickEvent(target) {
+  const itemName = target.parentNode.childNodes[1].childNodes[7].innerText;
+  const itemPrice =
+    target.parentNode.childNodes[1].childNodes[9].innerText.split(" ")[0];
+
+  const li = document.createElement("li");
+
+  // Increment the count for each new item
+  count++;
+
+  // Add classes and content to the list item and items prices
+  li.classList.add("m-1", "pl-2");
+  li.innerHTML = `${count}. ${itemName} ${itemPrice} TK`;
+
+  const productList = document.getElementById("product-list");
+  productList.appendChild(li);
+}

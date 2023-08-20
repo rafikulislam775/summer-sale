20099b972ffe985ff6bf185a1c7f65debd137b3c
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-08-20 22:15:44 +0900

    adding discount function and total

--- script.js
+++ script.js
@@ -23,4 +23,21 @@ function clickEvent(target) {
   totalPrice += itemPrice;
   const totalPriceElement = document.getElementById("total-price");
   totalPriceElement.innerText = `${totalPrice.toFixed(2)} TK`;
+
+  //   adding coupon code
 }
+
+document.getElementById("coupon-btn").addEventListener("click", function () {
+  const coupon = document.getElementById("coupon");
+  if (coupon.value === "SELL200") {
+    const coupon = document.getElementById("coupon");
+    if (coupon.value === "SELL200") {
+      const discountField = document.getElementById("discount-field");
+      const discount = (totalPrice * 20) / 100;
+      discountField.innerText = discount.toFixed(2);
+      const total = document.getElementById("total");
+      total.innerText = (totalPrice - discount).toFixed(2);
+      console.log(discount);
+    }
+  }
+});

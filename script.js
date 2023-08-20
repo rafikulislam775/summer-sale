c0aa6c56cdd0b2677d8b36a83d050d6ab6891e74
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-08-20 21:31:22 +0900

    adding the total price with functioin

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let count = 0; // Initialize the count
+let itemCount = 0; // Initialize the item count
+let totalPrice = 0;
 
 function clickEvent(target) {
   const itemName = target.parentNode.childNodes[1].childNodes[7].innerText;
-  const itemPrice =
-    target.parentNode.childNodes[1].childNodes[9].innerText.split(" ")[0];
+  const itemPrice = parseFloat(
+    target.parentNode.childNodes[1].childNodes[9].innerText.split(" ")[0]
+  );
 
   const li = document.createElement("li");
 
-  // Increment the count for each new item
-  count++;
+  // Increment the item count for each new item
+  itemCount++;
 
   // Add classes and content to the list item and items prices
   li.classList.add("m-1", "pl-2");
-  li.innerHTML = `${count}. ${itemName} ${itemPrice} TK`;
+  li.innerHTML = `${itemCount}. ${itemName} ${itemPrice} TK`;
 
   const productList = document.getElementById("product-list");
   productList.appendChild(li);
+
+  // Update the total price
+  totalPrice += itemPrice;
+  const totalPriceElement = document.getElementById("total-price");
+  totalPriceElement.innerText = `${totalPrice.toFixed(2)} TK`;
 }
